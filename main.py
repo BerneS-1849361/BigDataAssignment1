991e748b958ae6d928030f2d6cdd09e2e15bef96
Berne <sannen.berne@gmail.com>
2021-10-11 14:47:08 +0200

    Pass 2 PCY

--- main.py
+++ main.py
@@ -2,28 +2,33 @@ import itertools
 import xml.sax
 
 
-class AuthorHandler(xml.sax.ContentHandler):
+class Pass1(xml.sax.ContentHandler):
     def __init__(self, threshold):
         self.author = ""
-        self.key = ""
+        self.authors = []
+
+        self.threshold = threshold
+        self.bucketSize = 10000
 
         self.authorCount = {}
-        self.pairs = []
-        self.threshold = threshold
+        self.buckets = [0] * self.bucketSize
 
     # Call when an element starts
     def startElement(self, tag, attributes):
         self.CurrentData = tag
         if tag == "article":
-            key = attributes["key"]
+            self.authors = []
 
     # Call when an elements ends
     def endElement(self, tag):
         if self.CurrentData == "author":
+            self.authors.append(self.author)
             if self.author in self.authorCount:
                 self.authorCount[self.author] += 1
             else:
                 self.authorCount[self.author] = 1
+        if tag == "article":
+            self.fillBucket(2)
         self.CurrentData = ""
 
     # Call when a character is read
@@ -31,19 +36,96 @@ class AuthorHandler(xml.sax.ContentHandler):
         if self.CurrentData == "author":
             self.author = content
 
-    def generatePairs(self):
-        frequentItems = {}
-        for key in self.authorCount:
-            if self.authorCount[key] >= self.threshold:
-                frequentItems[key] = self.authorCount[key]
-        self.pairs = itertools.combinations(frequentItems.items(), 2)
-        print(len(list(self.pairs)))
+    def fillBucket(self,  k):
+        tupleArray = list(itertools.combinations(self.authors, k))
+        for tuple in tupleArray:
+            index = self.hash(tuple)
+            self.buckets[index] += 1
 
+    def getBucketAsBitvector(self):
+        bitVec = 0
+        count = 0
+        for i in self.buckets:
+            if i >= self.threshold:
+                bitVec |= 1 << count
+            else:
+                bitVec |= 0 << count
+            count += 1
+        return bitVec
+
+    def hash(self, tuple):
+        string = ""
+        for name in tuple:
+            string += name
+
+        string = string.encode('utf-8')
+        return int.from_bytes(string, 'little') % self.bucketSize
+
+
+    def filterFrequentAuthors(self):
+        frequentAuthors = {}
+        for author in self.authorCount:
+            if self.authorCount[author] >= self.threshold:
+                frequentAuthors[author] = self.authorCount[author]
+        self.authorCount = frequentAuthors
+
+
+
+class PassN(xml.sax.ContentHandler):
+    def __init__(self, passCount, threshold, bitvector, frequentAuthors):
+        self.passCount = passCount
+        self.frequentAuthors = frequentAuthors
+
+        self.author = ""
+        self.key = ""
+
+        self.authorTuples = {}
+        self.threshold = threshold
+
+        self.bitvector = bitvector
+
+        self.bucketSize = 10000
+        self.buckets = [0] * self.bucketSize
+
+    # Call when an element starts
+    def startElement(self, tag, attributes):
+        self.CurrentData = tag
+        if tag == "article":
+            self.authors = []
+
+    # Call when an elements ends
+    def endElement(self, tag):
+        if self.CurrentData == "author":
+            self.authors.append(self.author)
+
+        if tag == "article":
+            AuthourCombinationArray = list(itertools.combinations(self.authors, self.passCount))
+            for combination in AuthourCombinationArray:
+                string = ""
+                frequent = True
+                for name in combination:
+                    if name not in self.frequentAuthors:
+                        frequent = False
+                    string += name
+
+                if not frequent:
+                    break
+                string = string.encode('utf-8')
+                index = int.from_bytes(string, 'little') % self.bucketSize
+                if self.bitvector & (1 << index):
+                    if not combination in self.authorTuples:
+                        self.authorTuples[combination] = 1
+                    else:
+                        self.authorTuples[combination] += 1
+        self.CurrentData = ""
+
+    # Call when a character is read
+    def characters(self, content):
+        if self.CurrentData == "author":
+            self.author = content
 
 
 if __name__ == '__main__':
-    file1 = open("dblp.txt", "w")
-
     source = "dblp50000.xml"
 
     # create an XMLReader
@@ -52,10 +134,16 @@ if __name__ == '__main__':
     parser.setFeature(xml.sax.handler.feature_namespaces, 0)
 
     # override the default ContextHandler
-    handler = AuthorHandler(10)
+    handler = Pass1(10)
     parser.setContentHandler(handler)
 
+    #parse file with the first pass
     parser.parse(source)
-    handler.generatePairs()
 
-    file1.close()
+    print(handler.authorCount['Dennis Shasha'])
+    handler.filterFrequentAuthors()
+    nhandler = PassN(2, 10, handler.getBucketAsBitvector(), handler.authorCount)
+    parser.setContentHandler(nhandler)
+    parser.parse(source)
+
+    print(nhandler.authorTuples)
